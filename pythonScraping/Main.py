3d1edee1f43dec2efba4486d3f984eb34210baa8
Yuu Hikida <ponponda0103@gmail.com>
2024-07-21 14:59:14 +0900

    get できました

--- pythonScraping/Main.py
+++ pythonScraping/Main.py
@@ -11,5 +11,13 @@ response = request.urlopen(url)
 soup = BeautifulSoup(response, "html.parser")
 response.close()
 
-# HTMLの内容をプリティプリント
-print(soup.prettify())
+# コントリビューション情報を抽出
+contributions = soup.find_all("td", class_="ContributionCalendar-day")
+
+# 各日付毎にコントリビューション数を表示
+print("contributions:")
+for day in contributions:
+    date = day.get("data-date")
+    count = day.get("data-level")  # data-levelはコントリビューション数のレベルを表す
+    if int(count) > 0:  # コントリビューションがあった場合
+        print(date, ":", count)
