02b86b0e9892ee630585020d90a113b4ea262cd7
Yuu Hikida <ponponda0103@gmail.com>
2024-07-24 23:26:42 +0900

    main関数を定義

--- pythonScraping/Main.py
+++ pythonScraping/Main.py
@@ -2,40 +2,45 @@ import os
 from urllib import request
 from bs4 import BeautifulSoup
 from datetime import datetime
+def main():
+    #DBに入っている全ユーザーの"username"を取得して回す
+    # GitHubのユーザー名
+    username = "YuuHikida"
+    url = "https://github.com/users/" + username + "/contributions"
+    response = request.urlopen(url)
 
-# GitHubのユーザー名
-username = "YuuHikida"
-url = "https://github.com/users/" + username + "/contributions"
-response = request.urlopen(url)
+    # BeautifulSoup初期化
+    soup = BeautifulSoup(response, "html.parser")
+    response.close()
 
-# BeautifulSoup初期化
-soup = BeautifulSoup(response, "html.parser")
-response.close()
+    # 現在の日付を取得 (YYYY-MM-DDの形式)
+    today = datetime.today().strftime('%Y-%m-%d')
+    print(f"Today's date: {today}")
 
-# 現在の日付を取得 (YYYY-MM-DDの形式)
-today = datetime.today().strftime('%Y-%m-%d')
-print(f"Today's date: {today}")
+    # コントリビューション情報を抽出
+    contributions = soup.find_all("td", class_="ContributionCalendar-day")
 
-# コントリビューション情報を抽出
-contributions = soup.find_all("td", class_="ContributionCalendar-day")
+    # 今日のコントリビューションがあったかどうかを示すフラグ
+    found_today = False
 
-# 今日のコントリビューションがあったかどうかを示すフラグ
-found_today = False
+    # 各日付毎にコントリビューション数を確認
+    for day in contributions:
+        date = day.get("data-date")
+        if date is not None:
+            date = date.strip()
+            if date == today:
+                # contributeを取得
+                count = int(day.get("data-level"))
+                if count > 0:
+                    found_today = True
+                break  # 今日の日付を見つけたらループを抜ける
 
-# 各日付毎にコントリビューション数を確認
-for day in contributions:
-    date = day.get("data-date")
-    if date is not None:
-        date = date.strip()
-        print(f"Scraped date: {date}")  # 取得した日付を表示
-        if date == today:
-            count = int(day.get("data-level"))
-            if count > 0:  # コントリビューションがあった場合
-                found_today = True
-            break  # 今日の日付を見つけたらループを抜ける
+    # 今日のコントリビューションがあったかどうかを表示
+    if found_today:
+        print(f"今日はコントリビューションがあります 日付: {today}")
+    else:
+        print(f"今日はコントリビューションがありません 日付: {today}")
+        #以下　DBからidをjavaプログラムへ飛ばす
 
-# 今日のコントリビューションがあったかどうかを表示
-if found_today:
-    print(f"今日はコントリビューションがあります 日付: {today}")
-else:
-    print(f"今日はコントリビューションがありません 日付: {today}")
+if __name__ == "__main__":
+    main()
