0e4c7f1ff63bb9a5d68a5616be9117cf84464669
Yuu Hikida <ponponda0103@gmail.com>
2024-07-29 21:30:36 +0900

    処理を変えた

--- pythonScraping/Main.py
+++ pythonScraping/Main.py
@@ -25,7 +25,7 @@ def main():
 
     # 現在の日付を取得 (YYYY-MM-DDの形式)
     today = datetime.today().strftime('%Y-%m-%d')
-    print(f"Today's date: {today}")
+    print(f"本日の日付は: {today}")
 
     # コントリビューション情報を抽出
     contributions = soup.find_all("td", class_="ContributionCalendar-day")
@@ -36,24 +36,45 @@ def main():
     # 各日付毎にコントリビューション数を確認
     contribution_dates = []
 
+    # contributeがある日だけリストに入れる
     for day in contributions:
-        date = day.get("data-date")
-        if date is not None:
-            date = date.strip()
-            # 日付をリストに追加
-            contribution_dates.append(date)
-            if date == today:
-                # contributeを取得
-                count = int(day.get("data-level"))
-                if count > 0:
-                    found_today = True
-                break  # 今日の日付を見つけたらループを抜ける
+        count = int(day.get("data-level"))
+        if count > 0:
+            date = day.get("data-date")
+            if date is not None:
+                date = date.strip()
+                # 日付をリストに追加
+                contribution_dates.append(date)
+
+    # for day in contributions:
+    #     date = day.get("data-date")
+    #     if date is not None:
+    #         date = date.strip()
+    #         # 日付をリストに追加
+    #         contribution_dates.append(date)
+    #         if date == today:
+    #             # contributeを取得
+    #             count = int(day.get("data-level"))
+    #             if count > 0:
+    #                 found_today = True
+    #             break  # 今日の日付を見つけたらループを抜ける
 
     # 日付をソート
     contribution_dates.sort()
-    print("Sorted contribution dates:")
+    print("以下Contributeがある日付のみをソート表示:¥n")
     for date in contribution_dates:
         print(f"{date}")
+
+    # 本日のContributeがあったか表示
+    for push in contribution_dates:
+        if push is not None:
+            date = push.strip()
+            if date == today:
+                # contributeを取得
+                count = int(push.get("data-level"))
+                if count > 0:
+                    found_today = True
+                break  # 今日の日付を見つけたらループを抜ける
 
     # 今日のコントリビューションがあったかどうかを表示
     if found_today:
@@ -62,7 +83,7 @@ def main():
         print(f"今日はコントリビューションがありません 日付: {today}")
         # 以下　DBからidをjavaプログラムへ飛ばす
         call_java_file()
-
+    print("------------------------------------------------")
 
 if __name__ == '__main__':
     main()
