102d0045699cc79f0fd121ccdef5ee1e572d83f0
Yuu Hikida <ponponda0103@gmail.com>
2024-08-09 20:44:02 +0900

    mongoDB altなんとかに接続変えました

--- pythonScraping/DatabaseQuery.py
+++ pythonScraping/DatabaseQuery.py
@@ -1,16 +1,17 @@
-
 from pymongo.mongo_client import MongoClient
 from pymongo.server_api import ServerApi
 
-uri = "mongodb+srv://root:<password>@gitinfocontributes.btqfi.mongodb.net/?retryWrites=true&w=majority&appName=gitInfoContributes"
 
-# Create a new client and connect to the server
-client = MongoClient(uri, server_api=ServerApi('1'))
+def db_call():
+    uri = ("mongodb+srv://root:<password>@gitinfocontributes.btqfi.mongodb.net/?retryWrites=true&w=majority&appName"
+           "=gitInfoContributes")
 
-# Send a ping to confirm a successful connection
-try:
-    client.admin.command('ping')
-    print("Pinged your deployment. You successfully connected to MongoDB!")
-except Exception as e:
-    print(e)
+    # Create a new client and connect to the server
+    client = MongoClient(uri, server_api=ServerApi('1'))
 
+    # Send a ping to confirm a successful connection
+    try:
+        client.admin.command('ping')
+        print("Pinged your deployment. You successfully connected to MongoDB!")
+    except Exception as e:
+        print(e)
