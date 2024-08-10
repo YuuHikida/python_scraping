f36752c334dcc7ce711fe141b1825214546ace33
Yuu Hikida <ponponda0103@gmail.com>
2024-08-10 14:06:20 +0900

    MongoDB altas sucessfully connected

--- pythonScraping/DatabaseQuery.py
+++ pythonScraping/DatabaseQuery.py
@@ -1,17 +1,57 @@
+import os
+from urllib.parse import quote_plus
+from dotenv import load_dotenv
 from pymongo.mongo_client import MongoClient
 from pymongo.server_api import ServerApi
 
 
 def db_call():
-    uri = ("mongodb+srv://root:<password>@gitinfocontributes.btqfi.mongodb.net/?retryWrites=true&w=majority&appName"
-           "=gitInfoContributes")
+    # .envファイルを読み込む
+    load_dotenv()
+    username = os.getenv('USERNAME')
+    password = os.getenv('PASSWORD')
 
-    # Create a new client and connect to the server
+    # ユーザー名とパスワードをエスケープ
+    escaped_username = quote_plus(username)
+    escaped_password = quote_plus(password)
+
+    uri = (f"mongodb+srv://{escaped_username}:{escaped_password}@gitinfocontributes.btqfi.mongodb.net/?retryWrites"
+           f"=true&w=majority&appName=gitInfoContributes")
     client = MongoClient(uri, server_api=ServerApi('1'))
 
-    # Send a ping to confirm a successful connection
     try:
         client.admin.command('ping')
         print("Pinged your deployment. You successfully connected to MongoDB!")
     except Exception as e:
         print(e)
+
+
+# import os
+# from dotenv import load_dotenv
+# from urllib.parse import quote_plus
+# from pymongo import MongoClient
+#
+#
+# def db_call():
+#     # .envファイルを読み込む
+#     load_dotenv()
+#     username = os.getenv('USERNAME')
+#     password = os.getenv('PASSWORD')
+#
+#     # ユーザー名とパスワードをエスケープ
+#     escaped_username = quote_plus(username)
+#     escaped_password = quote_plus(password)
+#
+#     # 接続文字列を作成
+#     connection_url = (f"mongodb+srv://{escaped_username}:{escaped_password}@cluster.mongodb.net/myDatabase?retryWritestrue&w=majority")
+#
+#     # tls=TrueはMongoDB atlatsheへの接続をTLS(SSL)を有効にするオプション
+#     client = MongoClient(connection_url, tls=True)
+#     # client = MongoClient(connection_url, tls=True, tlsCertificateKeyFile='/path/to/certificate.pem')
+#
+#     db = client['testDB']
+#     collection = db['testCol']
+#     doc_count = collection.count_documents({})
+#     print(doc_count)
+#
+#
