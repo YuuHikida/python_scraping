185b0c8dde07e39afbd211111bfc4117bdd92427
Yuu Hikida <ponponda0103@gmail.com>
2024-08-17 17:26:02 +0900

    koreha seikou

--- Main.py
+++ Main.py
@@ -7,26 +7,28 @@ from GetContributes import get_contribute_main
 
 @functions_framework.http
 def run_batch(request):
-    try:
-        # DBに問い合わせ
-        client, collection = db_call()
-
-        if client is not None and collection is not None:
-            # クエリでsortされたUser情報格納変数
-            document_count, documents = db_read(collection)
-
-            print(f"取得したドキュメントの数は: {document_count}")
-            # 同期的スクレイピング実行
-            get_contribute_main(documents)
-
-            # DB閉じる
-            client.close()
-
-            return f"{documents}", 200
-
-        else:
-            return "error: Failed to connect to database", 500
-
-    except Exception as e:
-        return f"{e}", 500
-
+    name = request.args.get("name", "World")
+    return f"Hello, {name}"
+    #
+    # try:
+    #     # DBに問い合わせ
+    #     client, collection = db_call()
+    #
+    #     if client is not None and collection is not None:
+    #         # クエリでsortされたUser情報格納変数
+    #         document_count, documents = db_read(collection)
+    #
+    #         print(f"取得したドキュメントの数は: {document_count}")
+    #         # 同期的スクレイピング実行
+    #         get_contribute_main(documents)
+    #
+    #         # DB閉じる
+    #         client.close()
+    #
+    #         return f"{documents}", 200
+    #
+    #     else:
+    #         return "error: Failed to connect to database", 500
+    #
+    # except Exception as e:
+    #     return f"{e}", 500
